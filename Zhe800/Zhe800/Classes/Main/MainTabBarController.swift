383bf8e172de17160fb2007992baf1786f78a689
SpongeBob-GitHub <1605319671@qq.com>
2015-09-02 10:50:51 +0800

    1.2 调整界面框架布局+icon+launchScreen

--- Zhe800/Zhe800/Classes/Main/MainTabBarController.swift
+++ Zhe800/Zhe800/Classes/Main/MainTabBarController.swift
@@ -32,27 +32,43 @@ class MainTabBarController: UITabBarController {
     
     func addControllers() {
         
-        addchildController("Home", "首页", "home_tab_home_btn", "home_tab_home_selected_btn")
-        addchildController("Message", "积分商城", "home_tab_point_btn", "home_tab_point_selected_btn")
-        addchildController("Me", "个人中心", "home_tab_personal_btn", "home_tab_personal_selected_btn")
+        addChildController("Home", "首页", "home_tab_home_btn", "home_tab_home_selected_btn")
+        addViewController("Message", "分类", "home_tab_saunter_btn", "home_tab_saunter_selected_btn")
+        addChildController("Message", "品牌团", "home_tab_branc_btn", "home_tab_branc_selected_btn")
+        addChildController("Message", "积分商城", "home_tab_point_btn", "home_tab_point_selected_btn")
+        addViewController("Me", "个人中心", "home_tab_personal_btn", "home_tab_personal_selected_btn")
         
     }
     
     // Add child Controller
-    func addchildController(name: String, _ title: String, _ imageName: String, _ highlight: String) {
+    func addChildController(name: String, _ title: String, _ imageName: String, _ highlight: String) -> Void {
         
         let sb = UIStoryboard(name: name, bundle: nil)
         let vc = sb.instantiateInitialViewController() as! UINavigationController
         
         vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.imageInsets = UIEdgeInsetsMake(5, 0, -5.0, 0)
         vc.tabBarItem.selectedImage = UIImage(named: highlight)?.imageWithRenderingMode(.AlwaysOriginal)
-        
-        vc.title = title
-        
+        vc.tabBarItem.title = title
         vc.navigationItem.title = title
+        
+        vc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor(red: 198/255.0, green: 60/255.0, blue: 85/255.0, alpha:1)], forState: UIControlState.Selected)
+
+        self.addChildViewController(vc)
+    }
+    
+    // add ViewController
+    func addViewController(name: String, _ title: String, _ imageName: String, _ highlight: String) -> Void {
+        let sb = UIStoryboard(name: name, bundle: nil)
+        let vc = sb.instantiateInitialViewController() as! UIViewController
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: highlight)?.imageWithRenderingMode(.AlwaysOriginal)
+        vc.tabBarItem.imageInsets = UIEdgeInsetsMake(5, 0, -5.0, 0)
+        vc.tabBarItem.title = title
         
         vc.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor(red: 198/255.0, green: 60/255.0, blue: 85/255.0, alpha:1)], forState: UIControlState.Selected)
         
         self.addChildViewController(vc)
     }
+
 }
