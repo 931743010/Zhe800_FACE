617c469d75cd6d18d60eb34e1eec0950fbcbd79a
SpongeBob-GitHub <1605319671@qq.com>
2015-09-02 08:40:34 +0800

    init

---
+++ Zhe800_FACE/Zhe800_FACE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zhe800_FACE
+//
+//  Created by Bing Ma on 9/2/15.
+//  Copyright (c) 2015 Bing Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
